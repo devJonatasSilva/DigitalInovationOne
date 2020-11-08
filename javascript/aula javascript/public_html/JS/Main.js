868cb29b2921dd1c736d114df9ef53a51364b445
Jhony Smith <jonysilvaguitarplayer@gmail.com>
2020-11-08 16:25:18 -0300

    foi realisada uma organisação nos arquivos, por tanto este é o primeiro commit...

---
+++ javascript/aula javascript/public_html/JS/Main.js
@@ -0,0 +1,13 @@
+
+    /*
+    var senha = 130290;
+    var nome = "Jonatas";
+    alert("meu nome é: " + nome + " e minha senha é; " + senha);
+    alert(nome.replace("Jonatas", "Pedro"));
+*/
+
+var lista = ["Jonatas","Sevgerino","Silva"];
+lista.push("melão");
+console.log(lista[1]);
+alert(lista[2]);
+alert(lista.length);

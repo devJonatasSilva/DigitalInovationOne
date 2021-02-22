49fda7f6cdc53e2a86831bca06c70964b5953c94
jony <jony@jony-HP-ProBook-430-G2>
2021-02-22 14:09:04 -0300

    incluida a auala de colections e criadas as classes referentes as aulas

---
+++ java/java/src/Implementando_Collections_e_Streams_com_Java/Ultilizando_o_Optional.java
@@ -0,0 +1,18 @@
+/*
+ *o objetivo desta aula é entendercomo funciona o optional, 
+ *sendo o optional uma boa forma de se trabalhar com valores nulos
+ *  o optional possui 2 estados:
+        * presente
+        * nulo
+ *  permite que se trabalhe com valores que podem ser nulos,
+sem se preocupar com as famosas NullPointExeptions.
+ */
+package Implementando_Collections_e_Streams_com_Java;
+
+/**
+ *
+ * @author jony
+ */
+public class Ultilizando_o_Optional {
+    
+}

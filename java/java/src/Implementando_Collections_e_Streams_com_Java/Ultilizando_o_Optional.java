145f4320725bc3dbabbf45cd3de7faf68c6abb8c
jony <jony@jony-HP-ProBook-430-G2>
2021-02-24 00:19:52 -0300

    trabalhando na classe Utilizando_o_Optional...

--- java/java/src/Implementando_Collections_e_Streams_com_Java/Ultilizando_o_Optional.java
+++ java/java/src/Implementando_Collections_e_Streams_com_Java/Ultilizando_o_Optional.java
@@ -7,12 +7,36 @@
  *  permite que se trabalhe com valores que podem ser nulos,
 sem se preocupar com as famosas NullPointExeptions.
  */
+
+/*
+                    exercicio final
+
+1- criar um optional de um determinado tipo de dado
+    ° crie com estado vazio, estado presente, e com null
+    ° se presente exiba o valor no console
+    ° se vazio, lance uma exeção IlegalStateExeption
+    ° se vazio exiba "Optional nvakzio" no console
+    ° se presente, trasforme o valor e exiba no console
+    ° se presente, pegue o valor do optional e atribua em uma variavel
+    ° se presente filtre o optional com uma determinada regra de negocio
+*/
 package Implementando_Collections_e_Streams_com_Java;
+import java.util.Optional;
+
+import java.util.Optional;
 
 /**
  *
  * @author jony
  */
 public class Ultilizando_o_Optional {
+    
+    public static void main(String[] args){
+        
+        Optional<String> optionalString = Optional.of("valor presente");
+        
+        System.out.println("valor opcional está presente");
+        optionalString.ifPresentOrElse(System.out.println, () -> System.out.println("não está presente"));
+    }
     
 }

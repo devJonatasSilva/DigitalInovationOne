9e8057502845d05de031c677c7db15d69b30d0a9
devJonatasSilva@gmail.com <>
2021-04-22 14:33:17 -0300

    testando o codigo de interface funcionais

---
+++ java/Desenvolvimento_avancado_Java/src/interfaces_funcionais/FuncaoAltaOrdem.java
@@ -0,0 +1,25 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package interfaces_funcionais;
+
+/**
+ *
+ * @author jony_
+ */
+public class FuncaoAltaOrdem {
+    
+    public static void main(String[] args){
+        Soma soma = (numero1, numero2) -> numero1 * numero2; 
+        System.out.println(realizarSoma(soma, 10,100));
+    }
+    public static int realizarSoma(Soma soma, int numero1, int numero2){
+        return soma.somar(numero1, numero2);
+    }    
+}
+
+interface Soma{
+    public int somar(int numero1, int numero2);
+}
